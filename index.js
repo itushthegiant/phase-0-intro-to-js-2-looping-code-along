8ae850a8a296ff3018da40ac12b6dc034cd8626e
etamar <etamar.levy@gmail.com>
2021-02-15 21:34:28 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let arrayOfThanks = [];
+    for (let i = 0; i < names.length; i++) {
+        arrayOfThanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return arrayOfThanks;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
+
+function countDown() {
+    let counter = 10;
+    while (counter >= 0) {
+        console.log(counter);
+        counter--;
+    }
+}
+
+countDown();
